338e67c97b23409eb25d0a706588e2cedee16d85
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-27 16:47:26 -0300

    test(unit): app render logo and btn mode

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => render(<App />));
+
+test('renders react logo', () => {
+  const logo = screen.getByAltText(/logo React/i);
+  expect(logo).toBeInTheDocument();
+});
+
+test('renders btn mode', () => {
+  const btn = screen.getByTestId('btn-mode');
+  expect(btn).toBeInTheDocument();
 });
