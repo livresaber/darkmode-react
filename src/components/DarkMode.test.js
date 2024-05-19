2322b17fcbb71c2852229a408827c6057d934d30
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-19 18:38:22 -0300

    update act from react

--- src/components/DarkMode.test.js
+++ src/components/DarkMode.test.js
@@ -1,25 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-import DarkMode from './DarkMode';
+import { act } from "react";
+import { render, screen } from "@testing-library/react";
+import DarkMode from "./DarkMode";
 
 beforeEach(() => render(<DarkMode />));
 
-test('renders dark mode', () => {
-  const btn = screen.getByTestId('btn-mode');
+test("renders dark mode", () => {
+  const btn = screen.getByTestId("btn-mode");
   expect(btn).toBeInTheDocument();
 
   const darkmode = screen.getByText(/DarkMode/i);
   expect(darkmode).toBeInTheDocument();
-  expect(JSON.parse(localStorage.getItem('darkmode'))).toBeTruthy();
-  expect(document.body.classList.contains('darkmode')).toBeTruthy();
+  expect(JSON.parse(localStorage.getItem("darkmode"))).toBeTruthy();
+  expect(document.body.classList.contains("darkmode")).toBeTruthy();
 });
 
-test('renders light mode', () => {
-  const btn = screen.getByTestId('btn-mode');
-  act(() => btn.dispatchEvent(new MouseEvent('click', { bubbles: true })));
+test("renders light mode", () => {
+  const btn = screen.getByTestId("btn-mode");
+  act(() => btn.dispatchEvent(new MouseEvent("click", { bubbles: true })));
 
   const lightmode = screen.getByText(/LightMode/i);
   expect(lightmode).toBeInTheDocument();
-  expect(JSON.parse(localStorage.getItem('darkmode'))).toBeFalsy();
-  expect(document.body.classList.contains('lightmode')).toBeTruthy();
+  expect(JSON.parse(localStorage.getItem("darkmode"))).toBeFalsy();
+  expect(document.body.classList.contains("lightmode")).toBeTruthy();
 });
